29651c30ccd6d2f9e0034804520942c4a5e27246
chase <yun.er.run@gmail.com>
2016-08-06 22:09:09 +0800

    Basic runloop

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 // Package main provides entry for the command line tool
 package main
 
-func main() {
+import (
+	"github.com/shanzi/wu/runner"
+	"os"
+	"path/filepath"
+)
 
+func main() {
+	var path string
+	if len(os.Args) > 1 {
+		path, _ = filepath.Abs(os.Args[1])
+	} else {
+		path, _ = filepath.Abs(filepath.Dir(os.Args[0]))
+	}
+	r := runner.NewRunner(path, []string{}, "")
+	r.Start()
 }
